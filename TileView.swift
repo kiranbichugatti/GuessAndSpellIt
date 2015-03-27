5227b56258d6699abe131ec31f60bcdc83563a80
Mingyu Ma <magicptmmy@gmail.com>
2015-03-27 11:47:53 -0500

    letters can be displayed on tile

--- TileView.swift
+++ TileView.swift
@@ -24,9 +24,20 @@ class TileView:UIImageView {
         
         let scale = sideLength / image.size.width
         self.frame = CGRect(x: 0, y: 0, width: image.size.width * scale, height: image.size.height * scale)
+        
+        //add a letter on top
+        let letterLabel = UILabel(frame: self.bounds)
+        letterLabel.textAlignment = NSTextAlignment.Center
+        letterLabel.textColor = UIColor.blackColor()
+        letterLabel.backgroundColor = UIColor.clearColor()
+        letterLabel.text = String(letter).uppercaseString
+        letterLabel.font = UIFont(name: "Verdana-Bold", size: 78.0*scale)
+        self.addSubview(letterLabel)
     }
 
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    
 }
