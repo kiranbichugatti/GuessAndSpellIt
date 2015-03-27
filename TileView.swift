fefbcfb465b70b7ecf26c231da222054c90e761b
Mingyu Ma <magicptmmy@gmail.com>
2015-03-27 10:45:08 -0500

    added TileView class and a tile image

--- TileView.swift
+++ TileView.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+import UIKit
+
+class TileView:UIImageView {
+    
+    var letter:Character
+    
+    init(letter:Character) {
+        self.letter = letter
+        
+        let image = UIImage(named:"tile")!
+        super.init(image:image)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
