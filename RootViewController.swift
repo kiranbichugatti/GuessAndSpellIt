a7c514dd98737ac9b5e03af19558cfe294acc4cf
kiranbichugatti <kiran.bichugatti@gmail.com>
2015-04-16 13:10:34 -0500

    background music

--- RootViewController.swift
+++ RootViewController.swift
@@ -7,14 +7,58 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RootViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    var backgroundMusicPlayer: AVAudioPlayer!
+    
+       func playBackgroundMusic(filename: String) {
+                let url = NSBundle.mainBundle().URLForResource(
+                    filename, withExtension: nil)
+                if (url == nil) {
+                        println("Could not find file: \(filename)")
+                        return
+                    }
         
-
-        // Do any additional setup after loading the view.
-    }
+                var error: NSError? = nil
+                backgroundMusicPlayer =
+                    AVAudioPlayer(contentsOfURL: url, error: &error)
+             if backgroundMusicPlayer == nil {
+                        println("Could not create audio player: \(error!)")
+                        return
+                    }
+        
+                backgroundMusicPlayer.numberOfLoops = -1
+                backgroundMusicPlayer.prepareToPlay()
+                backgroundMusicPlayer.play()
+            }
+        func pauseBackgroundMusic()
+        {
+            if(backgroundMusicPlayer.playing == true)
+            {
+                backgroundMusicPlayer.stop()
+                backgroundMusicPlayer.currentTime = 0
+            }
+            else
+            {
+            backgroundMusicPlayer.numberOfLoops = -1
+                backgroundMusicPlayer.prepareToPlay()
+                backgroundMusicPlayer.play()
+    
+            }
+        }
+    
+    
+            
+    
+        
+          override func viewDidLoad() {
+            
+                    playBackgroundMusic("titlescreen.mp3")
+            
+            // Do any additional setup after loading the view.
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
