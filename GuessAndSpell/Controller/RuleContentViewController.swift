b71141992cb60117392cd4c73a207f672866afd4
Mingyu Ma <magicptmmy@gmail.com>
2015-05-06 21:08:34 -0500

    almost final

---
+++ GuessAndSpell/Controller/RuleContentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RuleContentViewController.swift
+//  GuessAndSpell
+//
+//  Created by uicsi8 on 5/6/15.
+//  Copyright (c) 2015 uics1. All rights reserved.
+//
+
+import UIKit
+
+class RuleContentViewController: UIViewController {
+
+    @IBAction func homeButtonTouched() {
+        self.navigationController?.popToRootViewControllerAnimated(true)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
---
+++ GuessAndSpell/Controller/RuleHintsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RuleHintsViewController.swift
+//  GuessAndSpell
+//
+//  Created by uicsi8 on 5/6/15.
+//  Copyright (c) 2015 uics1. All rights reserved.
+//
+
+import UIKit
+
+class RuleHintsViewController: UIViewController {
+
+    @IBAction func backButtonTouched() {
+        self.navigationController?.popViewControllerAnimated(true)
+    
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
